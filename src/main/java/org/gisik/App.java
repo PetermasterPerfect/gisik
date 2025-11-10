c021256040152cb5f04f378ba2f0df5f86533c0c
PetermasterPerfect <petermasterperfect@gmail.com>
2025-11-10 15:43:08 +0100

    I added very basic features to main window. Draft of menu bar, common staff like file, edit etc. For now only "Add vector" works which loads shape file. Shape file is displayed on the screen. User can move map view with mouse and zoom display with mouse wheel.

--- src/main/java/org/gisik/App.java
+++ src/main/java/org/gisik/App.java
@@ -1,7 +1,27 @@
 package org.gisik;
+import org.geotools.api.data.FileDataStore;
+import org.geotools.api.data.FileDataStoreFinder;
+import org.geotools.api.data.SimpleFeatureSource;
+import org.geotools.api.style.Style;
+import org.geotools.map.FeatureLayer;
+import org.geotools.map.Layer;
+import org.geotools.map.MapContent;
+import org.geotools.map.MapViewport;
+import org.geotools.styling.SLD;
+import org.geotools.swing.JMapPane;
+import org.geotools.swing.control.JMapStatusBar;
+import org.geotools.swing.data.JFileDataStoreChooser;
+import org.geotools.swing.tool.*;
+
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.File;
+import java.io.IOException;
 
 public class App  extends JFrame {
+
+    private final MapContent mapContent;
 
     private JMenu createFileMenu() {
         JMenu menu;
@@ -30,17 +50,33 @@ public class App  extends JFrame {
 
     private JMenu createLayerMenu() {
         JMenu menu;
-        JMenuItem addLayerItem = new JMenuItem("Add layer");
+        JMenuItem addVectorItem = new JMenuItem(new AbstractAction("Add Vector") {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    File file = JFileDataStoreChooser.showOpenFile("shp", null);
+                    if (file == null) {
+                        return;
+                    }
+                    FileDataStore store = FileDataStoreFinder.getDataStore(file);
+                    SimpleFeatureSource featureSource = store.getFeatureSource();
+                    Style style = SLD.createSimpleStyle(featureSource.getSchema());
+                    Layer layer = new FeatureLayer(featureSource, style);
+                    mapContent.addLayer(layer);
+                }
+                catch (IOException ex) {
+                    System.err.println(ex.toString());
+                }
+            }
+        });
+        JMenuItem addCsvItem = new JMenuItem("Add csv");
         menu = new JMenu("Layer");
-        menu.add(addLayerItem);
+        menu.add(addVectorItem);
+        menu.add(addCsvItem);
         return menu;
     }
 
     private void createMenu() {
         JMenuBar menuBar;
-        JMenu fileMenu, editMenu, viewMenu, layerMenu;
-        JMenuItem save, saveItem;
-        JCheckBoxMenuItem cbMenuItem;
 
         menuBar = new JMenuBar();
 
@@ -52,6 +88,14 @@ public class App  extends JFrame {
         this.setJMenuBar(menuBar);
     }
 
+    private void setupPanAndZoom(JMapPane mapPane) {
+        PanTool panTool = new PanTool();
+        panTool.setMapPane(mapPane);
+        mapPane.setCursorTool(panTool);
+        mapPane.addMouseListener(new ScrollWheelTool(mapPane));
+
+    }
+
     public App() {
         createMenu();
         setTitle("gisik");
@@ -59,6 +103,19 @@ public class App  extends JFrame {
         setSize(800, 600);
         setLocationRelativeTo(null);
         setVisible(true);
+        mapContent = new MapContent();
+        MapViewport viewport = new MapViewport();
+        viewport.setFixedBoundsOnResize(true);
+        mapContent.setViewport(viewport);
+        JMapPane mapPane = new JMapPane();
+
+        setupPanAndZoom(mapPane);
+        mapPane.setMapContent(mapContent);
+        this.setLayout(new BorderLayout());
+        this.add(mapPane, BorderLayout.CENTER);
+        JPanel panel = new JPanel();
+        panel.add(JMapStatusBar.createDefaultStatusBar(mapPane), "grow");
+        this.getContentPane().add(panel, BorderLayout.SOUTH);
     }
 
     public static void main(String[] args) {
